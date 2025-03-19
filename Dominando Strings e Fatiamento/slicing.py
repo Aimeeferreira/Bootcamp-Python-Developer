17339588654bc1fa3e4284095f6b0fe6421010be
Aimée Ferreira <aimeeferreira19@gmail.com>
2025-03-18 21:06:35 -0300

    up slicing

---
+++ Dominando Strings e Fatiamento/slicing.py
@@ -0,0 +1,10 @@
+name = "Aimée Stéfanie Santos Ferreira"
+
+print(name[0])
+print(name[-2])
+print(name[:9])
+print(name[10:])
+print(name[10:16])
+print(name[10:16:2])
+print(name[:])
+print(name[::-1])
