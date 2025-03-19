97a4ee002849478e6078df0efdd721226691815c
Aimée Ferreira <aimeeferreira19@gmail.com>
2025-03-18 21:05:46 -0300

    up multiple_lines

---
+++ Dominando Strings e Fatiamento/multiple_lines.py
@@ -0,0 +1,23 @@
+name = "Guilherme"
+
+message = f"""
+   Hello! My name is {name},
+ I'm learning Python.
+     This message has different indentations.
+"""
+
+print(message)
+
+print(
+    """
+    ============= MENU =============
+
+    1 - Deposit
+    2 - Cash out
+    0 - Exit
+
+    ================================
+
+            Thank you for using our system!!!!
+"""
+)
